6de7e58dae8663bd3895557732c1dc051abde513
ViniRamozz <robertinhoff46@gmail.com>
2026-09-07 15:09:19 +0000

    ex8

---
+++ achados-unifio/src/app/lista-exercicios/ex8/ex8.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex8',
+  standalone: false,
+  templateUrl: './ex8.html',
+  styleUrl: './ex8.scss',
+})
+export class Ex8 {
+  produto: string = 'Mouse Gamer';
+  preco: number = 150;
+  quantidade: number = 2;
+
+  calcularTotal(): number {
+    return this.preco * this.quantidade;
+  }
+}
