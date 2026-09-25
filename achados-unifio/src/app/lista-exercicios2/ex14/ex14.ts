70e5f03a45614c990292df0d6b07ac16edaa7afb
ViniRamozz <robertinhoff46@gmail.com>
2026-09-25 01:25:48 +0000

    lista-2

--- achados-unifio/src/app/lista-exercicios2/ex14/ex14.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ex14',
-  standalone: false,
-  templateUrl: './ex14.html',
-  styleUrl: './ex14.scss',
-})
-export class Ex14 {}
--- achados-unifio/src/app/lista-exercicios2/lista-exercicios2-module.ts
+++ achados-unifio/src/app/lista-exercicios2/lista-exercicios2-module.ts
@@ -7,18 +7,10 @@ import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
 import { Ex5 } from './ex5/ex5';
-import { Ex6 } from './ex6/ex6';
-import { Ex7 } from './ex7/ex7';
-import { Ex8 } from './ex8/ex8';
-import { Ex9 } from './ex9/ex9';
-import { Ex10 } from './ex10/ex10';
-import { Ex11 } from './ex11/ex11';
-import { Ex12 } from './ex12/ex12';
-import { Ex13 } from './ex13/ex13';
-import { Ex14 } from './ex14/ex14';
+
 
 @NgModule({
-  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5, Ex6, Ex7, Ex8, Ex9, Ex10, Ex11, Ex12, Ex13, Ex14],
+  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5],
   imports: [CommonModule, ListaExercicios2RoutingModule],
 })
 export class ListaExercicios2Module {}
